55ccfc01f9a1b2883ca34228e4f8eea3701b21ce
Tunji Bankole <shubbywax@gmail.com>
2025-07-11 10:37:56 +0100

    code the newsletter section component

---
+++ src/components/Newsletter.jsx
@@ -0,0 +1,16 @@
+import './styles/home.css';
+
+const Newsletter = () => {
+  return (
+    <section className="newsletter">
+      <h2>Join Our Wellness Community</h2>
+      <p>Subscribe to receive tips and special offers</p>
+      <form className="newsletter-form">
+        <input type="email" placeholder="Your email address" required />
+        <button type="submit" className="primary-button">Subscribe</button>
+      </form>
+    </section>
+  );
+};
+
+export default Newsletter;
